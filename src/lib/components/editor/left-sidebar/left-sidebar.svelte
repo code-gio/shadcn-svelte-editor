9932d52a4ee8900024061826e4e35f7fa39259f0
urioncode <giovani@urioncode.com>
2024-07-03 14:19:48 -0600

    added theming

--- src/lib/components/editor/left-sidebar/left-sidebar.svelte
+++ src/lib/components/editor/left-sidebar/left-sidebar.svelte
@@ -1,9 +1,19 @@
 <script>
 	import ComponentsList from './components-list.svelte';
+	import { SwatchBook } from 'lucide-svelte';
 </script>
 
-<div class="flex w-72 flex-col border-e border-border/80 bg-background/95 xl:w-80">
-	<div class="flex items-center justify-between p-6">
-		<ComponentsList />
-	</div>
+<div class="flex w-72 flex-col space-y-6 border-e border-border/80 bg-background/95 p-6 xl:w-80">
+	<ul class="space-y-3">
+		<li>
+			<a
+				class="active-link group flex items-center gap-x-2 text-sm font-semibold capitalize text-foreground/70 hover:text-blue-500 dark:text-neutral-400 dark:hover:text-neutral-300"
+				href="/theming"
+			>
+				<SwatchBook class="size-5" strokeWidth={1.5} />
+				Theming
+			</a>
+		</li>
+	</ul>
+	<ComponentsList />
 </div>
